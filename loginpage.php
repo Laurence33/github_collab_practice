aaf5e339bce9e773968475b82bce4c5c7382547a
britneygrace <calulot19britney@gmail.com>
2022-10-21 22:51:34 +0800

    initial login page

---
+++ loginpage.php
@@ -0,0 +1,13 @@
+<?php ?>
+<div class="row">
+    <i class="fas fa-user"></i>
+    <input type="email" name="e-mail" placeholder="Email" required>
+</div>
+<div class="row">
+    <i class="fas fa-lock"></i>
+    <input type="password" name="pword" placeholder="Password" required>
+</div>
+<div class="pass"><a href="#">Forgot password?</a></div>
+<div class="row button">
+    <input type="submit" value="Login">
+</div>
